582447735554c6e319c22645cd08b2639dd317c1
axstav <axstav94@gmail.com>
2020-09-04 21:26:07 -0500

    3_Permisos CORS

--- src/main/java/com/tumi/controller/UtilController.java
+++ src/main/java/com/tumi/controller/UtilController.java
@@ -3,14 +3,17 @@ package com.tumi.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.tumi.dto.ResultadoList;
 import com.tumi.service.UtilServiceImpl;
 
+@CrossOrigin(origins = "*", methods= {RequestMethod.GET,RequestMethod.POST})
 @RestController
 @RequestMapping("/util")
 public class UtilController {
--- src/main/java/com/tumi/controller/VisionPersonaQuecController.java
+++ src/main/java/com/tumi/controller/VisionPersonaQuecController.java
@@ -1,14 +1,17 @@
 package com.tumi.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.tumi.dto.VisionPersonaQuec;
 import com.tumi.service.VisionPersonaQuecServiceImpl;
 
+@CrossOrigin(origins = "*", methods= {RequestMethod.GET,RequestMethod.POST})
 @RestController
 @RequestMapping("/vpersonaquec")
 public class VisionPersonaQuecController {
